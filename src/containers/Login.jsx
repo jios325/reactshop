b780dc79914034521e13b6eef1de4ad0968c4b3e
Carlos Salgado <soporteweb@oasishoteles.com>
2023-01-16 16:29:29 -0500

    jsx

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -2,22 +2,18 @@ import React from "react";
 
 const Login = () => {
     return (
-        <div className="login">
-            <div className="form-container">
-            <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo">
-
-            <h1 className="title">Create a new password</h1>
-            <p className="subtitle">Enter a new passwrd for yue account</p>
-
-            <form action="/" className="form">
-                <label for="password" className="label">Password</label>
-                <input type="password" id="password" placeholder="*********" className="input input-password">
-
-                <label for="new-password" className="label">Password</label>
-                <input type="password" id="new-password" placeholder="*********" className="input input-password">
-
-                <input type="submit" value="Confirm" className="primary-button login-button">
-            </form>
+       <div classname="login">
+            <div classname="form-container">
+                <img src="./logos/logo_yard_sale.svg" alt="logo" classname="logo" />
+                <h1 classname="title">Create a new password</h1>
+                <p classname="subtitle">Enter a new passwrd for yue account</p>
+                <form action="/" classname="form">
+                    <label htmlFor="password" classname="label">Password</label>
+                    <input type="password" id="password" placeholder="*********" classname="input input-password" />
+                    <label htmlFor="new-password" classname="label">Password</label>
+                    <input type="password" id="new-password" placeholder="*********" classname="input input-password" />
+                    <input type="submit" defaultValue="Confirm" classname="primary-button login-button" />
+                </form>
             </div>
         </div>
     );
